213546db93d986cb9baec603ad7552bfea033213
Karol Gutierrez <kjgs.22@gmail.com>
2020-03-10 07:24:39 -0600

    Remove comment

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -38,7 +38,6 @@ public class DataServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String lang = request.getParameter("lang");
-        // System.out.println(lang);
         Query query = new Query("Comment").addSort("timestamp", SortDirection.ASCENDING);
 
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
